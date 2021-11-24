cc09f0a2390d7b225fdbd36b967159335ede453d
Rodney van den Velden <rodneymichael2002@gmail.com>
2021-11-24 10:33:40 +0100

    reinit

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Client, Intents, Collection } = require('discord.js');
+const { readdirSync } = require('fs');
+const { token, prefix } = require('./config.json');
+
+const client = new Client({ intents: new Intents(32767) });
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+client.categories = readdirSync('./commands/');
+
+['command'].forEach(handler => {
+	require(`./handlers/${handler}`)(client);
+});
+
+client.once('ready', () => {
+	console.log(`${client.user.username} is ready!`);
+
+	client.user.setPresence({
+		status: 'online',
+		game: {
+			name: 'porn',
+			type: 'STREAMING'
+		}
+	});
+});
+
+client.once('messageCreate', async (message) => {
+	if (message.author.bot) return;
+	if (!message.guild) return;
+	if (!message.content.startsWith(prefix)) return;
+	if (!message.member) message.member = await message.guild.members.fetchMember(message);
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+
+	if (cmd.length === 0) return;
+
+	let command = client.commands.get(cmd);
+	if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+	if (command) command.run(client, message, args);
+});
+
+client.login(token);
